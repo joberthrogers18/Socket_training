4f95e76433779d045e65a816a994918038cb92e9
Joberth Rogers <joberth.rogers18@gmail.com>
2020-05-31 17:49:58 -0300

    Added route to show details of match
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- scoreboard/routes/index.js
+++ scoreboard/routes/index.js
@@ -4,6 +4,8 @@ var router = express.Router();
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
+const _ = require('lodash');
+
 // Set up the database
 const adapter = new FileSync('data/db.json');
 const db = low(adapter);
@@ -14,6 +16,14 @@ db.defaults(require('../data/default-data.json')).write();
 router.get('/', function(req, res, next) {
   const matches = db.get('matches').value();
   res.render('index', { matches });
+});
+
+router.get('/match/:id', function(req, res, next) {
+  const { id } = req.params;
+  const matches = db.get('matches').value();
+  const match = db.get('matches['+id+']').value()
+  match.bids = _.orderBy(match.bids, ['half', 'time'], ['desc', 'desc']);
+  res.render('match', { matches, match, id });
 });
 
 module.exports = router;
