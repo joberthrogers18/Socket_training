16176f36645303a9ab3e91bf0811b8c85d8061a8
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-01 22:44:28 -0300

    Added socket io setup
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- socket-chat-example/server.js
+++ socket-chat-example/server.js
@@ -1,7 +1,20 @@
 const app = require("./src/");
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 3333;
+
+io.on("connection", socket => {
+  console.log("User is connected!");
+
+  socket.on('chat-message', (msg) => {
+    console.log('message: ' + msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
 http.listen(PORT, () => {
   console.log(`Server running at port: ${PORT}`);
--- socket-chat-example/src/controllers/mainChatController.js
+++ socket-chat-example/src/controllers/mainChatController.js
@@ -1,6 +1,5 @@
 module.exports = {
   index(req, res) {
     return res.render('index');
-    // return res.json({hello: 'world'});
   }
 };
