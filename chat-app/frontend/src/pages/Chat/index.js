78a4d837a6a58d84890259241611ee777fc9ec4f
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-14 00:11:00 -0300

    Minor fixes
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- chat-app/frontend/src/pages/Chat/index.js
+++ chat-app/frontend/src/pages/Chat/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MdSend } from "react-icons/md";
 
 import "./styles.css";
@@ -7,95 +7,102 @@ import Navbar from "../../components/Navbars";
 function Chat() {
   const [message, setMessage] = useState("");
 
+  useEffect(() => {
+    const messageContent = document.getElementById("content-wrapper");
+    messageContent.scrollTop = messageContent.scrollHeight;
+  }, []);
+
   return (
     <>
       <Navbar />
       <div className="chat">
         <div className="wrapper-chat">
           <div className="content">
-            <div className="position-message">
-              <div className="message-wrapper">
-                <div className="user-name">Joberth Rogers</div>
-                <div className="message-content">
-                  Many desktop publishing packages and web page editors now use
-                  Lorem Ipsum as their default model text, and a search for
-                  'lorem ipsum' will uncover many web sites still in their
-                  infancy. Various versions have evolved over the years,
-                  sometimes by accident, sometimes on purpose (injected humour
-                  and the like).
+            <div id="content-wrapper">
+              <div className="position-message">
+                <div className="message-wrapper">
+                  <div className="user-name">Joberth Rogers</div>
+                  <div className="message-content">
+                    Many desktop publishing packages and web page editors now
+                    use Lorem Ipsum as their default model text, and a search
+                    for 'lorem ipsum' will uncover many web sites still in their
+                    infancy. Various versions have evolved over the years,
+                    sometimes by accident, sometimes on purpose (injected humour
+                    and the like).
+                  </div>
                 </div>
+                <div className="decoration-message"></div>
               </div>
-              <div className="decoration-message"></div>
-            </div>
-            <div className="position-message">
-              <div className="message-wrapper">
-                <div className="user-name">Joberth Rogers</div>
-                <div className="message-content">
-                  Many desktop publishing packages and web page editors now use
-                  Lorem Ipsum as their default model text, and a search for
-                  'lorem ipsum' will uncover many web sites still in their
-                  infancy. Various versions have evolved over the years,
-                  sometimes by accident, sometimes on purpose (injected humour
-                  and the like).
+              <div className="position-message">
+                <div className="message-wrapper">
+                  <div className="user-name">Joberth Rogers</div>
+                  <div className="message-content">
+                    Many desktop publishing packages and web page editors now
+                    use Lorem Ipsum as their default model text, and a search
+                    for 'lorem ipsum' will uncover many web sites still in their
+                    infancy. Various versions have evolved over the years,
+                    sometimes by accident, sometimes on purpose (injected humour
+                    and the like).
+                  </div>
                 </div>
+                <div className="decoration-message"></div>
               </div>
-              <div className="decoration-message"></div>
-            </div>
-            <div className="position-message">
-              <div className="message-wrapper">
-                <div className="user-name">Joberth Rogers</div>
-                <div className="message-content">
-                  Many desktop publishing packages and web page editors now use
-                  Lorem Ipsum as their default model text, and a search for
-                  'lorem ipsum' will uncover many web sites still in their
-                  infancy. Various versions have evolved over the years,
-                  sometimes by accident, sometimes on purpose (injected humour
-                  and the like).
+              <div className="position-message">
+                <div className="message-wrapper">
+                  <div className="user-name">Joberth Rogers</div>
+                  <div className="message-content">
+                    Many desktop publishing packages and web page editors now
+                    use Lorem Ipsum as their default model text, and a search
+                    for 'lorem ipsum' will uncover many web sites still in their
+                    infancy. Various versions have evolved over the years,
+                    sometimes by accident, sometimes on purpose (injected humour
+                    and the like).
+                  </div>
                 </div>
+                <div className="decoration-message"></div>
               </div>
-              <div className="decoration-message"></div>
-            </div>
-            <div className="position-message">
-              <div className="message-wrapper">
-                <div className="user-name">Joberth Rogers</div>
-                <div className="message-content">
-                  Many desktop publishing packages and web page editors now use
-                  Lorem Ipsum as their default model text, and a search for
-                  'lorem ipsum' will uncover many web sites still in their
-                  infancy. Various versions have evolved over the years,
-                  sometimes by accident, sometimes on purpose (injected humour
-                  and the like).
+              <div className="position-message">
+                <div className="message-wrapper">
+                  <div className="user-name">Joberth Rogers</div>
+                  <div className="message-content">
+                    Many desktop publishing packages and web page editors now
+                    use Lorem Ipsum as their default model text, and a search
+                    for 'lorem ipsum' will uncover many web sites still in their
+                    infancy. Various versions have evolved over the years,
+                    sometimes by accident, sometimes on purpose (injected humour
+                    and the like).
+                  </div>
                 </div>
+                <div className="decoration-message"></div>
               </div>
-              <div className="decoration-message"></div>
-            </div>
-            <div className="position-message">
-              <div className="message-wrapper">
-                <div className="user-name">Joberth Rogers</div>
-                <div className="message-content">
-                  Many desktop publishing packages and web page editors now use
-                  Lorem Ipsum as their default model text, and a search for
-                  'lorem ipsum' will uncover many web sites still in their
-                  infancy. Various versions have evolved over the years,
-                  sometimes by accident, sometimes on purpose (injected humour
-                  and the like).
+              <div className="position-message">
+                <div className="message-wrapper">
+                  <div className="user-name">Joberth Rogers</div>
+                  <div className="message-content">
+                    Many desktop publishing packages and web page editors now
+                    use Lorem Ipsum as their default model text, and a search
+                    for 'lorem ipsum' will uncover many web sites still in their
+                    infancy. Various versions have evolved over the years,
+                    sometimes by accident, sometimes on purpose (injected humour
+                    and the like).
+                  </div>
                 </div>
+                <div className="decoration-message"></div>
               </div>
-              <div className="decoration-message"></div>
-            </div>
-            <div className="position-message">
-              <div className="message-wrapper">
-                <div className="user-name">Joberth Rogers</div>
-                <div className="message-content">
-                  Many desktop publishing packages and web page editors now use
-                  Lorem Ipsum as their default model text, and a search for
-                  'lorem ipsum' will uncover many web sites still in their
-                  infancy. Various versions have evolved over the years,
-                  sometimes by accident, sometimes on purpose (injected humour
-                  and the like).
+              <div className="position-message">
+                <div className="message-wrapper">
+                  <div className="user-name">Joberth Rogers</div>
+                  <div className="message-content">
+                    Many desktop publishing packages and web page editors now
+                    use Lorem Ipsum as their default model text, and a search
+                    for 'lorem ipsum' will uncover many web sites still in their
+                    infancy. Various versions have evolved over the years,
+                    sometimes by accident, sometimes on purpose (injected humour
+                    and the like).
+                  </div>
                 </div>
+                <div className="decoration-message"></div>
               </div>
-              <div className="decoration-message"></div>
             </div>
           </div>
           <div className="inputs">
