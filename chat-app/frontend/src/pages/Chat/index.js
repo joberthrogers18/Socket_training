df78a5a2a5823519246374edbbc3c142a82c89f3
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-13 22:34:09 -0300

    Styled chat page component
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- chat-app/frontend/src/pages/Chat/index.js
+++ chat-app/frontend/src/pages/Chat/index.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdSend } from "react-icons/md";
+
 import "./styles.css";
+import Navbar from "../../components/Navbars";
 
 function Chat() {
-  return <h1>Chat</h1>;
+  const [message, setMessage] = useState("");
+
+  return (
+    <>
+      <Navbar />
+      <div className="chat">
+        <div className="wrapper-chat">
+          <div className="content"></div>
+          <div className="inputs">
+            <input
+              type="text"
+              id=""
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button>
+              <MdSend color="#FFF" size="1.2em" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Chat;
